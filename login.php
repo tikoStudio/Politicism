c4b4a16f3b47fd36f157a30863d34b11cb0274df
tikoStudio <timothy.koenig@mail.com>
2020-07-07 15:12:56 +0200

    frontend register page completed

--- login.php
+++ login.php
@@ -13,40 +13,43 @@
 </head>
 
 <body>
-    <section class="logo__section">
-        <img class="wordLogo__img" src="images/wordLogo.svg" alt="Politicism word logo">
-        <div class="logo__box">
-            <img class="iconLogo__img" src="images/iconLogo.svg" alt="Politicism icon logo">
-        </div>
-    </section>
-
-    <section class="form__section">
-        <form class="form form--login" action="" method="post">
-
-            <?php if (isset($error)) : ?>
-            <div class="form__error">
-                <p>
-                    <?php echo $error; ?>
-                </p>
+    <div class="container--duo">
+        <section class="logo__section">
+            <img class="wordLogo__img" src="images/wordLogo.svg" alt="Politicism word logo">
+            <div class="logo__box">
+                <img class="iconLogo__img" src="images/iconLogo.svg" alt="Politicism icon logo">
             </div>
-            <?php endif; ?>
+        </section>
 
-            <input class="input__form" type="text" id="email" name="email" placeholder="Email">
+        <section class="form__section">
+            <form class="form form--login" action="" method="post">
+                <h2 class="form__heading" form-title>Welcome Politician!<br>Login</h2>
 
-            <input class="input__form" type="password" id="password" name="password" placeholder="Password">
+                <?php if (isset($error)) : ?>
+                <div class="form__error">
+                    <p>
+                        <?php echo $error; ?>
+                    </p>
+                </div>
+                <?php endif; ?>
 
-            <input class="btn" type="submit" value="Login!">
+                <input class="input__form" type="text" id="email" name="email" placeholder="Email">
 
-            <label class="form__loginState">Keep me logged in!
-                <input type="checkbox">
-                <span class="checkmark"></span>
-            </label>
-        </form>
-        <div class="page__switchers">
-            <h2 class="switch__pages">No account yet?</h2>
-            <h2 class="switch__pages orange"><a class="a__pages" href="register.php">Register here</a></h2>
-        </div>
-    </section>
+                <input class="input__form" type="password" id="password" name="password" placeholder="Password">
+
+                <input class="btn" type="submit" value="Login!">
+
+                <label class="form__loginState">Keep me logged in!
+                    <input type="checkbox">
+                    <span class="checkmark"></span>
+                </label>
+            </form>
+            <div class="page__switchers">
+                <h2 class="switch__pages">No account yet?</h2>
+                <h2 class="switch__pages orange"><a class="a__pages" href="register.php">Register here</a></h2>
+            </div>
+        </section>
+    </div>
 </body>
 
 </html>
--- register.php
+++ register.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Politicism - Register</title>
+    <title>Politicism - Login</title>
 
     <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;700&display=swap" rel="stylesheet">
 
@@ -13,7 +13,45 @@
 </head>
 
 <body>
+    <div class="container--duo">
+        <section class="logo__section">
+            <img class="wordLogo__img" src="images/wordLogo.svg" alt="Politicism word logo">
+            <div class="logo__box logo__box--none">
+                <img class="iconLogo__img" src="images/iconLogo.svg" alt="Politicism icon logo">
+            </div>
+        </section>
 
+        <section class="form__section">
+            <form class="form form--login" action="" method="post" enctype="multipart/form-data">
+
+                <?php if (isset($error)) : ?>
+                <div class="form__error">
+                    <p>
+                        <?php echo $error; ?>
+                    </p>
+                </div>
+                <?php endif; ?>
+
+                <label for="avatar"><img class="form__avatar" src="images/avatarUpload.svg" alt="upload avatar"></label>
+                <input type="file" class="form-control white" name="avatar" id="avatar" accept="image/*">
+
+                <input class="input__form" type="text" id="email" name="email" placeholder="Email">
+
+                <input class="input__form" type="text" id="username" name="username" placeholder="Username">
+
+                <input class="input__form" type="password" id="password" name="password" placeholder="Password">
+
+                <input class="input__form" type="password" id="confirmpassword" name="confirmpassword"
+                    placeholder="Confirm password">
+
+                <input class="btn" type="submit" value="Register!">
+            </form>
+            <div class="page__switchers">
+                <h2 class="switch__pages">Already have an account?</h2>
+                <h2 class="switch__pages orange"><a class="a__pages" href="login.php">Login here</a></h2>
+            </div>
+        </section>
+    </div>
 </body>
 
 </html>
